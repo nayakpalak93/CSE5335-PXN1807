447342cbc874c8a7a161bf1e6d51f690d58a17b4
rails -v <nayakpalak93@gmail.com>
2015-10-03 21:50:04 -0500

    new app

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,96 @@ class WelcomeController < ApplicationController
 
   # GET /welcome
   def index
+	end
 
-  end
+def movieList
+
+@movie = [
+{
+"name"=>"3 Idiots",
+"Director"=>"Rajkumar Hirani",
+"country"=>"India",
+"lattitude"=>"24.732338",
+"longitude"=>"75.396411"
+
+},
+{
+"name"=>"Wild Tales",
+"Director"=>"Damian Szifron",
+"country"=>"Argentina",
+"lattitude"=>"",
+"longitude"=>""
+
+},
+{
+"name"=>"The Way He Looks",
+"Director"=>"Daniel Ribeiro",
+"country"=>"Brazil",
+"lattitude"=>"",
+"longitude"=>""
+
+},
+{
+"name"=>"Glitch",
+"Director"=>"Emma Freeman",
+"country"=>"Australia",
+"lattitude"=>"",
+"longitude"=>""
+
+},
+{
+"name"=>"Turbo Kid",
+"Director"=>"Anouk Whissell",
+"country"=>"Canada",
+"lattitude"=>"",
+"longitude"=>""
+
+},
+{
+"name"=>"The Blue Elephant",
+"Director"=>"Marwan Hamed",
+"country"=>"Egypt",
+"lattitude"=>"",
+"longitude"=>""
+
+},
+{
+"name"=>"All American High",
+"Director"=>"Keva Rosenfeld",
+"country"=>"Finland",
+"lattitude"=>"",
+"longitude"=>""
+
+},
+{
+"name"=>"Hitman Agent47",
+"Director"=>"Aleksander Batch",
+"country"=>"Germany",
+"lattitude"=>"",
+"longitude"=>""
+
+},
+{
+"name"=>"Stop At Nothing",
+"Director"=>"Chris Thomson",
+"country"=>"Italy",
+"lattitude"=>"",
+"longitude"=>""
+
+},
+{
+"name"=>"Red Army",
+"Director"=>"Gabe Polsky",
+"country"=>"Russia",
+"lattitude"=>"",
+"longitude"=>""
+
+}
+];
+
+render json:@movie
+end
+
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :widgets
+  #resources :widgets
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
 
   root 'welcome#index'
+	get '/movielist' => 'welcome#movieList'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
